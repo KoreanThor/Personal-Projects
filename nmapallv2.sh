36c9b3894bafab63e32b44ef1933fdc379489a56
Heeyoon Jang <81708279+KoreanThor@users.noreply.github.com>
2022-03-05 12:48:22 -0800

    Updated to take user input for file path and filename

--- nmapallv2.sh
+++ nmapallv2.sh
@@ -1,16 +1,18 @@
 #!/bin/bash 
 
+#Initial Nmap Scan (flags can be changed based on preference) 
 echo 'Initial nmap scan beginning...' 
 echo 'Please enter the target IP address:'  
 read IP 
+#File Path (ex: /home/steven/Documents/htb/filename)
 echo 'Please enter your desired file path and filename:' 
 read filename 
 parent=$(dirname $filename) 
 nmap -sC -sV -O $IP -oA $filename  
-
+#All ports scan
 echo -e '\nAll ports scan beginning...This may take a while...' 
 nmap -p- -sV $IP --max-retries 4 
-
+#UDP scan
 echo -e '\nUDP scan beginning...This may take a while...' 
 nmap -sV -sU $IP --max-retries 4 
 
